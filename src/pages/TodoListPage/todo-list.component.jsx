ee1db14b3857c2f11626fe232b4d4d0bd754ef2c
Evia-H <evi1020@gmail.com>
2021-05-23 13:41:48 -0700

    added todo form

--- src/pages/TodoListPage/todo-list.component.jsx
+++ src/pages/TodoListPage/todo-list.component.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import Todo from "../../components/Todo/todo.component";
+import TodoForm from "../../components/TodoForm/todo-form.component";
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
 
     this.state = { todos: [{ task: "walk" }, { task: "dance" }] };
+    this.create = this.create.bind(this);
+  }
+
+  create(newTodo) {
+    this.setState({ todos: [...this.state.todos, newTodo] });
   }
 
   render() {
     const { todos } = this.state;
     return (
       <div className="todo-list">
+        <TodoForm createTodo={this.create} />
         <h1>Todo List!</h1>
         <ul>
           {todos.map(({ task }) => (
