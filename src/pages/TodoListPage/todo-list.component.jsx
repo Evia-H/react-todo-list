3f2b4a567ca47649d615ba2cbad056fa3c3f38b9
Evia-H <evi1020@gmail.com>
2021-05-24 10:20:46 -0700

    added api call with useEffect

--- src/pages/TodoListPage/todo-list.component.jsx
+++ src/pages/TodoListPage/todo-list.component.jsx
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from "react";
 import Todo from "../../components/Todo/todo.component";
 import TodoForm from "../../components/TodoForm/todo-form.component";
+import axios from "axios";
 import "./todo-list.styles.scss";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(1);
+  const [user, setUser] = useState("");
 
   useEffect(() => {
-    document.title = `you clicked ${count} times`;
-  });
+    async function getData() {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/todos/${number}/`
+      );
+      setUser(response.data);
+    }
+    getData();
+  }, [number]);
 
   const create = (newTodo) => {
     setTodos((todos) => [...todos, newTodo]);
@@ -48,6 +57,20 @@ const TodoList = () => {
       <div className="counter">
         <h1>you clicked:{count}</h1>
         <button onClick={() => setCount(count + 1)}>Click Me</button>
+      </div>
+
+      <div className="user-selector">
+        <h1>Pick A User</h1>
+        <h4>{user.title}</h4>
+        <select value={number} onChange={(e) => setNumber(e.target.value)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+        </select>
       </div>
     </div>
   );
