37b03a4c2b8b1304cf6a2b5cefdf6368f40e2147
Evia-H <evi1020@gmail.com>
2021-05-23 19:00:30 -0700

    added option to edit task, intreducing hooks

--- src/components/Todo/todo.component.jsx
+++ src/components/Todo/todo.component.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Todo = ({ id, task, removeTodo }) => (
-  <div className="todo">
-    <button>Edit</button>
-    <button onClick={() => removeTodo(id)}>X</button>
-    <li>{task}</li>
-  </div>
-);
+const Todo = ({ id, task, removeTodo, editTodo }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [todo, setTodo] = useState(task);
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    editTodo(id, todo);
+    setIsEdit(false);
+  };
+
+  const handleChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  return isEdit ? (
+    <div>
+      <form onSubmit={handleUpdate}>
+        <input type="text" onChange={handleChange} value={todo} />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  ) : (
+    <div className="todo">
+      <button onClick={() => setIsEdit(true)}>Edit</button>
+      <button onClick={() => removeTodo(id)}>X</button>
+      <li>{task}</li>
+    </div>
+  );
+};
 
 export default Todo;
--- src/pages/TodoListPage/todo-list.component.jsx
+++ src/pages/TodoListPage/todo-list.component.jsx
@@ -1,40 +1,44 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Todo from "../../components/Todo/todo.component";
 import TodoForm from "../../components/TodoForm/todo-form.component";
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
 
-    this.state = { todos: [] };
-    this.create = this.create.bind(this);
-    this.remove = this.remove.bind(this);
-  }
+  const create = (newTodo) => {
+    setTodos((todos) => [...todos, newTodo]);
+  };
 
-  create(newTodo) {
-    this.setState({ todos: [...this.state.todos, newTodo] });
-  }
+  const remove = (id) => {
+    setTodos((todos) => todos.filter((t) => t.id !== id));
+  };
 
-  remove(id) {
-    this.setState({
-      todos: this.state.todos.filter((t) => t.id !== id),
-    });
-  }
-
-  render() {
-    const { todos } = this.state;
-    return (
-      <div className="todo-list">
-        <TodoForm createTodo={this.create} />
-        <h1>Todo List!</h1>
-        <ul>
-          {todos.map(({ id, task }) => (
-            <Todo key={id} id={id} task={task} removeTodo={this.remove} />
-          ))}
-        </ul>
-      </div>
+  const editTodo = (id, task) => {
+    setTodos((todos) =>
+      todos.map((t) => {
+        if (t.id === id) return { ...t, task };
+        else return t;
+      })
     );
-  }
-}
+  };
+
+  return (
+    <div className="todo-list">
+      <TodoForm createTodo={create} />
+      <h1>Todo List!</h1>
+      <ul>
+        {todos.map(({ id, task }) => (
+          <Todo
+            key={id}
+            id={id}
+            task={task}
+            removeTodo={remove}
+            editTodo={editTodo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default TodoList;
